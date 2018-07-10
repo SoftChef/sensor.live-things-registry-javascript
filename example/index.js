b3c8c5e0afb86c33ef8bdba06fbdc7187abeeec9
MinChe Tsai <minche@softchef.com>
2018-07-10 12:51:24 +0800

    sync example

--- example/index.js
+++ example/index.js
@@ -1,24 +1,23 @@
 const aws_iot = require('aws-iot-device-sdk');
 const thing_registry = require('sensor.live-things-registry');
-const config = require('dotenv').config();
-
-const main = async () => {
-    thing_registry.setCertsPath('./certs');
-    if (!thing_registry.hasDeviceCertificate()) {
-        thing_registry.generateDeviceCertificate({
-            // thing_name: 'if you have thing name, you can set this property. Or use a unique name with your device private key'
-        });
+const config = {
+    aws_iot: {
+        endpoint: 'your aws iot endpoint',
+        port: '8883', // 8883 or 1883 is default supported with AWS IoT,
+        debug: false
     }
-    let thing_name = thing_registry.getThingName();
-    let keys_path = thing_registry.getKeysPath();
-    let client_id = `device-${thing_name}`
-    let thing_shadow = aws_iot.thingShadow({
-        ...keys_path,
-        host: process.env.AWS_IOT_HOST,
-        port: process.env.AWS_IOT_PORT,
-        debug: process.env.AWS_IOT_DEBUG === 'true',
-        clientId: client_id
-    });
-};
-
-main();
+}
+let thing_registry.setCertsPath('./certs'); // you can change the default certificates folder
+if (!thing_registry.hasDeviceCertificate()) {
+    thing_registry.generateDeviceCertificate();
+}
+let thing_name = thing_registry.getThingName();
+let keys_path = thing_registry.getKeysPath();
+let client_id = `device-${thing_name}`
+let thing_shadow = aws_iot.thingShadow({
+    ...keys_path,
+    host: config.aws_iot.endpoint,
+    port: config.aws_iot.port,
+    debug: config.aws_iot.debug,
+    clientId: client_id
+});
